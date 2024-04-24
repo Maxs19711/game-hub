419f0303d0ca3fdfb384dd6eff1dbe7867ced9f8
Maxs19711 <maximusthomas00@gmail.com>
2024-04-24 16:56:02 +0800

    Refactor- simplify game query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -15,10 +15,10 @@ interface Props {
   // 1. This is to notify the parent or the consumer of this component that a genre has been selected
   // the parent of this component is App.tsx component *go to App.tsx for next explaination
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -42,7 +42,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Button
                 whiteSpace={"normal"}
                 textAlign={"left"}
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
                 fontSize={"lg"}
                 variant={"link"}
